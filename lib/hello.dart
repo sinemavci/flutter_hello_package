980809bc6d50d50f63f0b34446b880645e501029
sinem.avci <sinem.avci@ekinokssoftware.com>
2022-04-28 17:50:47 +0300

    refactor: add new actions

--- lib/hello.dart
+++ lib/hello.dart
@@ -3,29 +3,46 @@ library hello;
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  var onPressed;
-  final Widget child;
-  var style;
-  CustomButton({
+  final Route route;
+
+  const CustomButton({
+    required this.route,
     Key? key,
-    @required this.onPressed,
-    required this.child,
-    this.style,
   }) : super(key: key);
+
+  void pushAndRemoveUntil(BuildContext context) {
+    Navigator.of(context).pushAndRemoveUntil(route, (route) => false);
+  }
+
+  Widget btnPushAndRemoveUntil(BuildContext context) {
+    return TextButton(
+      onPressed: () => pushAndRemoveUntil(context),
+      child: const Text("Push and Remove Until"),
+    );
+  }
+
+  Widget btnPopUntil(BuildContext context) {
+    return TextButton(
+      onPressed: () => pushAndRemoveUntil(context),
+      child: const Text("Pop Until"),
+    );
+  }
+
+  Widget btnPushReplacement(BuildContext context) {
+    return TextButton(
+      onPressed: () => pushAndRemoveUntil(context),
+      child: const Text("push replacement"),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-        onPressed: onPressed,
-        style: TextButton.styleFrom(
-          padding: const EdgeInsets.all(16.0),
-          primary: Colors.white,
-          backgroundColor: Colors.blue,
-          elevation: 9.0,
-          textStyle: const TextStyle(
-            fontSize: 20,
-          ),
-        ),
-        child: child);
+    return Column(
+      children: [
+        btnPushAndRemoveUntil(context),
+        btnPopUntil(context),
+        btnPushReplacement(context),
+      ],
+    );
   }
 }
